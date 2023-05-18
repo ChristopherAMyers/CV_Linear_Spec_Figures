91efdf32ca23f4b511c56d1414f970a6dc4c6080
Christopher Myers <myerschris92@gmail.com>
2023-05-17 20:13:33 -0700

    added h-bond plots

---
+++ PES/plot_both.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import plot_moh_acc, plot_moh_don
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+
+fontsize = 14
+rcParams = {
+    'xtick.labelsize': fontsize,
+    'ytick.labelsize': fontsize,
+    'font.weight': 'bold',
+    'axes.labelsize': fontsize + 2,
+    'axes.labelweight': 'extra bold',
+    'figure.labelsize': fontsize,
+    'figure.labelweight': 'bold',
+}
+plt.rcParams.update(rcParams)
+
+fig, (ax1, ax2) = plt.subplots(2, figsize=(6, 8))
+
+# left, bottom, width, height
+plot_moh_acc.plot_on_axes(ax1)
+plot_moh_don.plot_on_axes(ax2)
+
+#   add legend
+handles, labels = ax1.get_legend_handles_labels()
+fig.legend(handles, labels, loc='upper right', fontsize=fontsize, facecolor='white', framealpha=0.8, frameon=True, bbox_to_anchor=(0.2, 0.44, 0.5, 0.5), edgecolor='white')
+
+
+
+#   add image to plot
+im_donor = plt.imread('pymol_donor.png')
+im_acceptor= plt.imread('pymol_acceptor.png')
+
+#   x, y, width, height
+newax1 = fig.add_axes([0.122, 0.088 ,0.2,0.2], anchor='NE', zorder=1)
+newax1.imshow(im_donor)
+newax1.axis('off')
+newax2 = fig.add_axes([0.724, 0.8 ,0.25,0.2], anchor='NE', zorder=1)
+newax2.imshow(im_acceptor)
+newax2.axis('off')
+
+# ax1.text(0.9, 0.5, 'Amine as an Acceptor', transform=ax1.transAxes)
+
+
+fig.tight_layout()
+fig.savefig('both.png', dpi=500)
+plt.show()
---
+++ PES/plot_moh_don.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('../style.mplstyle')
+
+def plot_on_axes(ax, plot_legend=False):
+    data = np.loadtxt('moh_donor_data.txt').T
+    # titles = ['MM/MM-QUBEKit', 'MM/MM-QUBEKit(MK)', 'QM/MM (Solvated)', 'CAM-B3LYP/6-31G*']
+    titles = ['QUBEKit/QM1', 'QUBEKit(MK)/QM1', 'AIMD/QM1', 'AIMD/QM2']
+    colors = ['#BA6023', '#4D943C', '#E03223', '#0101F5']
+
+    for i, y in enumerate(data[1:]):
+        ax.plot(data[0], y/4.184, label=titles[i], color=colors[i])
+
+    ax.hlines(0, data[0].min(), data[0].max(), linestyle='--', color='k')
+    ax.set_xlim(1.3, 2.3)
+    ax.set_ylim(-16, 2)
+    ax.set_yticks(np.arange(-16, 2, 2))
+    ax.set_xlabel('N - H Distance (Ang.)')
+    ax.set_ylabel('Interaction Energy (kCal/mol)')
+    # ax.set_xticklabels(ax.get_xticks(), rotation=0, weight='bold', size=12)
+    if plot_legend:
+        ax.legend(ncol=2, fontsize=plt.rcParams['axes.labelsize']*0.9)
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots(figsize=(7, 5))
+    plot_on_axes(ax, True)
+    fig.tight_layout()
+    fig.savefig(__file__.replace('.py', '.png').replace('plot_', ''), dpi=300)
+    plt.show()
